560be8eba866e7308b4a0e492f1e10f48c52b52c
akpbadnawar <70686648+akpbadnawar@users.noreply.github.com>
2023-05-30 09:03:42 +0530

    adding scrollbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css'
+
 
 
 
@@ -32,15 +34,17 @@ render() {
     if (this.state.robots.length === 0) {
         return <h1>Loading</h1>
     } else{
-            return (
-                <div className="tc">
-                <h1 className="f1"> RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots= {filteredRobots}/>
-                </div>
-            );
-        }
-    }
+        return (
+        <div className="tc">
+        <h1 className="f1"> RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+            <CardList robots= {filteredRobots}/>
+        </Scroll>            
+        </div>
+        );
+    }   
+}
 }
 
 
