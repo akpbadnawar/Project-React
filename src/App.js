beb702f4474cb9eaa04d671b940a04b7dc5fdc9c
akpbadnawar <70686648+akpbadnawar@users.noreply.github.com>
2023-05-30 08:20:53 +0530

    adding API

--- src/App.js
+++ src/App.js
@@ -10,27 +10,36 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [], // user 'robots' for the dummy data
             searchfield: ''
         }
     }
 
-    onSearchChange = (event) => {
-        
-        this.setState({searchfield: event.target.value})
-        
-    }
+componentDidMount() {
+    this.setState({robots: robots});
+}
+
+onSearchChange = (event) => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users => this.setState({robots:users}))
+    };
 
 render() {
     const filteredRobots = this.state.robots.filter(robots => {return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) 
     })
-    return (
-            <div className="tc">
-            <h1 className="f1"> RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots= {filteredRobots}/>
-            </div>
-        );
+
+    if (this.state.robots.length === 0) {
+        return <h1>Loading</h1>
+    } else{
+            return (
+                <div className="tc">
+                <h1 className="f1"> RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots= {filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
 
